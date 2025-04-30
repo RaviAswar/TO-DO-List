444240940192ae12d50e2e3bd0e2eece696090b3
Ravi Aswar <raviaswar702@gmail.com>
2025-04-30 09:14:27 +0530

    TO-DO application is created by Ravi Aswar

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = 'mongodb://localhost:27017/todoapp';
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+app.use('/api', taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
